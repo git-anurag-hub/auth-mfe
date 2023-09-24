ff0a751713987e2100f3f72b03365ea97cb2f0d9
Anurag Gupta <me@anuragxd.com>
2023-09-25 00:53:09 +0530

    feat: auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "home",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./auth": "./src/components/Auth.js",
+        },
+        remotes: {},
+        shared: {},
+        extraOptions: {
+          exposePages: true,
+        },
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/auth.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Auth from "../components/Auth";
+
+const AuthPage = () => {
+  return (
+    <div>
+      <Auth>
+        {/* All System up properly */}
+        <h1>Auth Page</h1>
+      </Auth>
+    </div>
+  );
+};
+
+export default AuthPage;
