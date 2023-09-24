eee684c5847a65bb6ecee3bc9773bfb1f8c3715e
Anurag Gupta <me@anuragxd.com>
2023-09-25 01:15:10 +0530

    feat: auth

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Auth from "../components/Auth";
+// import Auth from "../components/Auth";
 
 const AuthPage = () => {
   return (
     <div>
-      <Auth>
-        {/* All System up properly */}
-        <h1>Auth Page</h1>
-      </Auth>
+      {/* <Auth> */}
+      {/* All System up properly */}
+      <h1>Auth Page</h1>
+      {/* </Auth> */}
     </div>
   );
 };
